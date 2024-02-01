dba585965b67af1c2b040e8aa6acd030d030a5ff
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 <ilovetrap123@hotmail.com>
2024-02-01 12:16:55 -0500

    initial commit

---
+++ src/main/java/com/github/wildsource/springbeangenerator/SpringBeanGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.wildsource.springbeangenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanGeneratorApplication.class, args);
+	}
+
+}

1774b7cdd9b4436ad22b98e7a8977780d325cc35
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 <ilovetrap123@hotmail.com>
2024-02-02 13:02:23 -0500

    refactor ServiceSrategy class

--- src/main/java/com/github/wildsource/springbeangenerator/app/strategies/ServiceStrategy.java
+++ src/main/java/com/github/wildsource/springbeangenerator/app/strategies/ServiceStrategy.java
@@ -2,30 +2,80 @@ package com.github.wildsource.springbeangenerator.app.strategies;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.lang.model.element.Modifier;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.javapoet.FieldSpec;
 import org.springframework.javapoet.JavaFile;
+import org.springframework.javapoet.MethodSpec;
 import org.springframework.javapoet.TypeSpec;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 public class ServiceStrategy implements Runnable {
 
+	private FieldSpec produceField() {
+		return FieldSpec.builder(Repository.class, "repository")
+						.addAnnotation(Autowired.class)
+						.addModifiers(Modifier.PRIVATE)
+						.build();
+	}
+
+	private MethodSpec produceReadMethod() {
+		return MethodSpec	.methodBuilder("readMockEntity")
+							.addParameter(Long.class, "id")
+							.returns(Object.class)
+							.addModifiers(Modifier.PRIVATE)
+							.build();
+	}
+
+	private MethodSpec produceCreateMethod() {
+		return MethodSpec	.methodBuilder("createMockEntity")
+							.addParameter(Object.class, "mock")
+							.returns(void.class)
+							.addModifiers(Modifier.PRIVATE)
+							.build();
+	}
+
+	private MethodSpec produceModifyMethod() {
+		return MethodSpec	.methodBuilder("modifyMockEntity")
+							.addParameter(Object.class, "modifiedMock")
+							.addParameter(Long.class, "targetMock")
+							.returns(void.class)
+							.addModifiers(Modifier.PRIVATE)
+							.build();
+	}
+
+	private MethodSpec produceDeleteMethod() {
+		return MethodSpec	.methodBuilder("deleteMockEntity")
+							.addParameter(Long.class, "id")
+							.returns(void.class)
+							.addModifiers(Modifier.PRIVATE)
+							.build();
+	}
+
+	private TypeSpec produceService(Iterable<MethodSpec> methods) {
+		return TypeSpec	.classBuilder("MockService")
+						.addModifiers(Modifier.PUBLIC)
+						.addAnnotation(Service.class)
+						.addField(produceField())
+						.addMethods(methods)
+						.build();
+	}
+
 	@Override
 	public void run() {
-		FieldSpec repository = FieldSpec.builder(Repository.class, "repository")
-										.addModifiers(Modifier.PRIVATE)
-										.build();
+		List<MethodSpec> methods = new ArrayList<MethodSpec>();
 
-		TypeSpec service = TypeSpec	.classBuilder("MockService")
-									.addModifiers(Modifier.PUBLIC)
-									.addAnnotation(Service.class)
-									.addField(repository)
-									.build();
+		methods.add(produceReadMethod());
+		methods.add(produceCreateMethod());
+		methods.add(produceModifyMethod());
+		methods.add(produceDeleteMethod());
 
-		JavaFile javaFile = JavaFile.builder("featureName", service)
+		JavaFile javaFile = JavaFile.builder("featureName", produceService(methods))
 									.build();
 
 		try {
